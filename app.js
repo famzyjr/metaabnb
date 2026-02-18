522b63aba17929a97586fd348bdd6d31261058a7
famzyjr <famuyiwaahmed132@gmail.com>
2026-02-18 18:50:45 +0100

    feat: implement connect wallet modal with overlay

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const sidebar = document.querySelector("header");
 const openBtn = document.getElementById("open-sidebar-btn");
 const closeBtn = document.getElementById("close-sidebar-btn");
+const walletBtn  = document.getElementById('wallet_btn')
+const Modal =  document.getElementById('modaloverlay');
+const Modalbtn =  document.getElementById('modal_btn')
+const ModalCloseBtn = document.getElementById('modal_close_btn');
+const modalInput = document.getElementById('modal_input');
+const modalSendBtn =  document.getElementById('modal_btn')
 
 openBtn.addEventListener("click", () => {
   sidebar.classList.add("active");
@@ -9,3 +15,30 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
+
+walletBtn.addEventListener('click', ()=>{
+ Modal.style.display = 'block';
+})
+
+ModalCloseBtn.addEventListener('click',()=>{
+  Modal.style.display = 'none';
+})
+
+
+
+const modalAlert =()=>{
+const inputValue = modalInput.value;
+if(inputValue.length >= 10){
+  alert('Your Wallet is Connected')
+ inputValue.innerText = '';
+}else{
+  alert('wallet ID is less than 10')
+}
+
+}
+
+modalSendBtn.onclick =()=> modalAlert()
+
+
+
+
