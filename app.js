29c7d33ad967b358ee81df834da3e0d30b473abe
famzyjr <famuyiwaahmed132@gmail.com>
2026-03-01 01:33:48 +0100

    I added heart icon to the imgs, Adjusted adventureOutput margin

--- app.js
+++ app.js
@@ -120,9 +120,13 @@ modalSendBtn.onclick =()=> modalAlert()
 
 const htmlOutput = AdventureLocation.map((items)=>{
  console.log(items.Amount);
-return  `  <div class="d">
+return  `<div class="d">
         <div class="adventurecard">
-        <img class="adventure_img" src=${items.imgs} alt="">
+        <div class="adventureImgContainer">
+         <img class="adventure_img" src=${items.imgs} alt="">
+      <div><img class="heartImg" src="Asset/Logos/heart.png" alt=""></div>
+      </div>
+      
        <div class="adventureContext">
         <div class="left_context">
              <p class="">${items.Location}</p>
